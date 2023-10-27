e598abc4bc449a55cd272ea4421e3c88bdc8ed06
mehabhatt <mehabhatt21@gmail.com>
2023-10-27 15:04:50 +0530

    Practical-10

--- app/src/main/java/com/example/mad_practical_10_21012011049/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011049/MainActivity.kt
@@ -4,20 +4,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var personListView : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        personListView = findViewById(R.id.listview_main)
-        val btn = findViewById<FloatingActionButton>(R.id.autorenew_btn)
+        val btn= findViewById<FloatingActionButton>(R.id.autorenew_btn)
+        btn.setOnClickListener(){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val data = HttpRequest().makeServiceCall(
+                        "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                        "dchj8v1b6qqdjzbqood1jgpachyfzlw58r540gru")
+                    withContext(Dispatchers.Main) {
+                        try {
+                            if(data != null)
+                                runOnUiThread{getPersonDetailsFromJson(data)}
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
-    fun setPersonData(){
-        personListView.adapter=PersonAdapter(this,
-            arrayListOf(
-                //Person("1", "John","John123@gmail.com", "9765431340","Ahmedabad",21.7051,72.9959),
 
+    private fun getPersonDetailsFromJson(sJson: String) {
+        val personList = ArrayList<Person>()
+        try {
+            val jsonArray = JSONArray(sJson)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray[i] as JSONObject
+                val person = Person(jsonObject)
+                personList.add(person)
+            }
+            val personListView=findViewById<ListView>(R.id.listview_main)
+            personListView.adapter = PersonAdapter(this, personList)
+        } catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
     }
+
+    /*fun setArraytoListView(){
+      val personListView=findViewById<ListView>(R.id.listview_main)
+        val array = arrayListOf<Person>(
+            Person("1", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 230117.83,723446.96),
+                    Person("1", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 172.455, 90.99),
+        Person("3", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 172.455, 90.9),
+        Person("4", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 172.455, 90.1),
+        Person("5", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 172.455, 90.2),
+        Person("6", "Meha Bhatt", "meha21@gmail.com", "9452321000", "S-21, Someshwar Flats, Ahemdabad", 172.455, 90.3)
+        )
+        personListView.adapter=PersonAdapter(this, array)
+    }*/
+
 }
 
--- app/src/main/java/com/example/mad_practical_10_21012011049/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011049/PersonAdapter.kt
@@ -13,6 +13,13 @@ import java.util.ArrayList
 class PersonAdapter(context:Context,  val Personarray : ArrayList<Person>): ArrayAdapter<Person>(context,0,Personarray) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view=LayoutInflater.from(context).inflate(R.layout.contact_item, parent, false)
+        view.findViewById<TextView>(R.id.name_textView).text = Personarray[position].name
+        view.findViewById<TextView>(R.id.number_textView).text= Personarray[position].phoneNo
+        view.findViewById<TextView>(R.id.email_textView).text= Personarray[position].emailId
+        view.findViewById<TextView>(R.id.address_textView).text= Personarray[position].address
+        view.findViewById<MaterialButton>(R.id.location_btn).setOnClickListener {
+            Intent(context, MapsActivity::class.java).putExtra("object",Personarray[position]).apply { context.startActivity(this) }
+        }
         return view
     }
 }
